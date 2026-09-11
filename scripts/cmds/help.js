175b2cdc585f0e2ea2d91f6ccc7475b9d1bd913c
shishir5499 <ayma54990@gmail.com>
2026-09-11 12:28:29 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -62,8 +62,8 @@ module.exports = {
     const categoriesList = global.GoatBot.cacheHelp;
 
     const videoURLs = [
-      "https://i.imgur.com/IudwgaP.mp4",
-      "https://i.imgur.com/AMv8IqG.mp4",
+      "https://i.imgur.com/T7azvEk.mp4",
+      "https://i.imgur.com/H01vGb2.mp4",
       "https://i.imgur.com/xhFp4Rc.mp4",
       "https://i.imgur.com/EXar1VY.mp4",
       "https://i.imgur.com/vWigmIF.mp4",
